39c6a31b9ac42729cba66237fcf7f85a363a4e46
Adrian Fager <49225203+adrianfager@users.noreply.github.com>
2020-02-06 15:50:07 +0100

    dsfgfddfg
    dfgdfggfd

--- FagersForbehandlingsFunktion.m
+++ FagersForbehandlingsFunktion.m
@@ -1,4 +1,17 @@
 TrumpC = fileread('trump.txt');
 trumpTXT = textscan(TrumpC,'%s','delimiter','\n'); 
-trump = trumpTXT{1};
+tweets = trumpTXT{1};
 
+tweets(2)
+tweets = lower(tweets);
+tweets(2)
+tweets = eraseTags(tweets);
+tweets(2)
+tweets = eraseURLs(tweets);
+tweets(2)
+
+torep = {'(?:\$+[\w_]+)?'};
+strip(regexprep(tweet,torep,''))
+tweets(2)
+
+tweets = tokenizedDocument(tweets);
