da653e2a582ed29b0fad4fa91ec70f5a9b3c3aca
parkerFrisk <efk15002@student.mdh.se>
2020-02-17 16:12:56 +0100

    Create termFrequency.m
    TF-IDF function

---
+++ termFrequency.m
@@ -0,0 +1,11 @@
+function [termFreqMatrix] = termFrequency(tokenDoc)
+%INPUT: Tokenized document
+%OUTPUt: matrix where each column is for each unique word in the document
+%and the rows are for each tweet. The number stands for the frequency of
+%that word in that tweet
+
+bag = bagOfWords(tokenDoc);
+termFreqMatrix = full(tfidf(bag,'TFWeight','raw','IDFWeight','normal'));
+
+end
+
